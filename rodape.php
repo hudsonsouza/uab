42b95af3fd7139b927f3be900c6252658bda0ff2
Hudson Souza <hudsonss@gmail.com>
2016-05-23 15:42:28 -0300

    rodape

--- rodape.php
+++ rodape.php
@@ -3,17 +3,17 @@
     
     <div>
         <center>
-            <span class="negrito">Sistema de Cálculos Epidemiológicos - SiEpi</span>
-            <br>Universidade Estadual de Maringá - UEM
-            <br>www.hudsonss.com.br/siepi - siepi@hudsonss.com.br
+            <span class="negrito">Polo EaD Paranavaí</span>
+            <br>Rua Barão do Cerro Azul, 210 - Jd. Santos Dumont - 87.706-560 - Paranavaí-PR
+            <br>www.paranavai.pr.gov.br/uab - uab@paranavai.pr.gov.br - +55 44 3902-1207
         </center>
     
         <div class="rodEsq">
-            &nbsp;&nbsp;&nbsp; by <b>Me. Hudson Souza</b> - hudson@hudsonss.com.br
+            &nbsp;&nbsp;&nbsp; by <b>Hudson Souza</b> - hudson@hudsonss.com.br
         </div>
 
         <div class="rodDir">
-            CopyRight &copy; 2016-<?echo date(Y)?>&nbsp;&nbsp;<a href='index.php?menu=ajuda-sobre' title="SiEpi 0.0.1" alt="SiEpi 0.0.1">| SiEpi - versão: 0.0.1 |</a>&nbsp;&nbsp;&nbsp;	
+            CopyRight &copy; 2007-<?echo date(Y)?> - Prefeitura do Município de Paranavaí &nbsp;&nbsp;&nbsp;	
         </div>
         
     </div>
