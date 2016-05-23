835792616f925630f9696a04804aed42b16743a2
Hudson Souza <hudsonss@gmail.com>
2016-05-23 16:40:25 -0300

    ead 2.0.2 - home

--- home.php
+++ home.php
@@ -1,10 +1,25 @@
 <?php ?>
 
-<br><br><br>
+<br>
+<div class="centro">
+<img src="img_int/uab.gif" alt="Universidade Aberta do Brasil - UAB" width="160" align="middle" title="Universidade Aberta do Brasil - UAB"></div>
 
-<center>
-    <h1>HOME</h1>
-    <br><br><br>
-    <h3>Página em Manutenção...</h3>
-    <br><br>
-</center>
+<br><br><p>O Projeto Universidade Aberta do Brasil (UAB) foi criado pelo Ministério da Educação, em 2005, no âmbito do Fórum das Estatais pela Educação.</p>
+
+<br><p>Para a articulação e integração de um sistema nacional de educação superior a distância, em caráter experimental, visando sistematizar as ações, programas, projetos, atividades pertencentes as políticas públicas voltadas para a ampliação e interiorização da oferta do ensino superior gratuito e de qualidade no Brasil.</p>
+<br>
+
+<br>
+<div class="centro">
+<span class="subtituloNormal">Rogério José Lorenzetti</span>
+<br/>Prefeito do Município de Paranavaí
+<br><br>
+<span class="subtituloNormal">Aparecida Silveira Gonçalves</span>
+<br/>Secretária Municipal de Educação
+<br><br>
+<span class="subtituloNormal">Josefa Bezerra Obana</span>
+<br/>
+Coordenadora do Pólo
+de Paranava&iacute; <br>
+</div>
+<br>
